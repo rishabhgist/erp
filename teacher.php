15f9116d65023911db843b130b83926f0fa05340
Rishabh Gist <damyrachit@gmail.com>
2020-04-02 19:25:58 +0530

    erp-php

--- teacher.php
+++ teacher.php
@@ -1,42 +1,67 @@
-<?php include 'functions.php'; ?>
+<?php include 'includes/function.php'; ?>
 
-<?php  ?>
+<?php 
+
+
+ ?>
 
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 
-	<title>Teacher Panel</title>
+	<title>Student Name</title>
 </head>
 <body>
-	<nav class="navbar navbar-expand bg-dark">
+	<nav class="navbar fixed-top navbar-expand">
 		<ul class="navbar-nav">
+			<li class="nav-item">
+			<img src="img/profile.jpg" class="profilePicNav">
+			</li>
 			<li class="nav-item active">
-				<a href="#home" class="nav-link">Home</a>
+				<a href="#home" class="nav-link">HOME</a>
 			</li>
 			<li class="nav-item">
-				<a href="#home" class="nav-link">Student</a>
+				<a href="#home" class="nav-link">NOTICE</a>
 			</li>
 			<li class="nav-item">
-				<a href="#home" class="nav-link">Teacher</a>
+				<a href="#home" class="nav-link">LIBRARY</a>
 			</li>
 			<li class="nav-item">
-				<a href="#home" class="nav-link">Report</a>
+				<a href="#home" class="nav-link">Q/A</a>
 			</li>
 			<li class="nav-item">
-				<a href="#home" class="nav-link">LogOut</a>
+				<a href="#home" class="nav-link">TIME TABLE</a>
 			</li>
-	
 		</ul>
 	</nav>
-<h3>Hello Teacher  </h2>
-<br>
-<?php 
-
-	
-
- ?>
-
+	<div class="coverContainer">
+		<img src="img/cover.jpg" class="coverImage center-fit">
+		<h3 class="head">Hello, Jhon</h3>
+		<h3 class="headDate">23 July, 2020 </h3>
+	</div>	
+	<br>
+	<div class="container">
+		<img src="img/profile.jpg" class="profilePic">
+		<a href="#">Change Profile</a><br>
+		<label>Basic Details</label>
+		<ul class="ul">
+		<li>Student ID : 2017BCA029 </li>
+		<li>Student Name : Jhon Doe</li>
+		<li>Father's Name : Jhon Wick</li>
+		<li>Date of Birth : 29 May 1999</li>
+		<li>Session form : 2017</li>
+		<li>Session to : 2020</li>
+		</ul>
+		<label>Communication Details</label>
+		<ul class="ul">
+		<li>Mobile : 7983294650 </li>
+		<li>Address : 21 Gagan Enclave, Rohta Road, Meerut </li>
+		<li>Email : jhodoe@gmail.com</li>
+		<li>Course : BCA </li>
+		<li>Password : <a href="#">Change Passowrd</a></li>
+		<li>Session to : 2020</li>
+		</ul>
+	</div>
 </body>
 </html>
