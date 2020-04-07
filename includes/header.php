7b1823cdecf9278ecbbee08e2f29a5df6b99da3d
Rishabh Gist <damyrachit@gmail.com>
2020-04-07 18:33:48 +0530

    update

--- includes/header.php
+++ includes/header.php
@@ -14,6 +14,7 @@ $lname = 'Doe';
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400&family=Ubuntu:ital,wght@0,300;1,300;1,500&display=swap" rel="stylesheet">
 
     <title><?php echo $fname.' '.$lname ?></title>
 </head>
--- profile.php
+++ profile.php
@@ -8,21 +8,37 @@ include 'includes/nav.php';
 ?>
 
 <div class="basic-details">
-
-	<div>
-        <span class="badge badge-primary">Basic Details</span>
-        <p>
-        	
-        	Name : Jhone Doe
-
-        </p>
-		
-
-
+	<div class="row">
+	<div class="profile-div">
+	<!-- <div class="col-md-10"> -->
+        <span>Basic Details</span>
+ <!--    </div> -->
+<!-- 
+    	<div class="col-md-2">
+    		<button class="btn-profile ">Edit Details</button>
+    	</div> -->
+    </div>
+   
+        	<!-- Details Here -->
+        </div>
 	</div>
-
-
-
-
  </div>
+
+ <div class="basic-details detail">
+	<div class="profile-div">
+        <span>Educational Details</span>
+        <div>
+        	<!-- details here -->
+        </div>
+	</div>
+ </div>
+<div class="basic-details detail">
+	<div class="profile-div">
+        <span>Communication Details</span>
+        <div>
+        	<!-- details here -->
+        </div>
+	</div>
+ </div>
+
 <?php include 'includes/footer.php'; ?>
